e2df3ea03b838a6f1f14493b3ab5418813ff0ff4
Pedro Curvo <pedro.mpcurvo@gmail.com>
2025-05-11 20:50:43 +0200

    feat: Implement MLP class with customizable activation functions and residual connections

---
+++ models/components/mlp.py
@@ -0,0 +1,32 @@
+import torch.nn as nn
+
+ACTIVATION = {'gelu': nn.GELU, 'tanh': nn.Tanh, 'sigmoid': nn.Sigmoid, 'relu': nn.ReLU, 'leaky_relu': nn.LeakyReLU(0.1),
+              'softplus': nn.Softplus, 'ELU': nn.ELU, 'silu': nn.SiLU}
+
+
+class MLP(nn.Module):
+    def __init__(self, n_input, n_hidden, n_output, n_layers=1, act='gelu', res=True):
+        super(MLP, self).__init__()
+
+        if act in ACTIVATION.keys():
+            act = ACTIVATION[act]
+        else:
+            raise NotImplementedError
+        self.n_input = n_input
+        self.n_hidden = n_hidden
+        self.n_output = n_output
+        self.n_layers = n_layers
+        self.res = res
+        self.linear_pre = nn.Sequential(nn.Linear(n_input, n_hidden), act())
+        self.linear_post = nn.Linear(n_hidden, n_output)
+        self.linears = nn.ModuleList([nn.Sequential(nn.Linear(n_hidden, n_hidden), act()) for _ in range(n_layers)])
+
+    def forward(self, x):
+        x = self.linear_pre(x)
+        for i in range(self.n_layers):
+            if self.res:
+                x = self.linears[i](x) + x
+            else:
+                x = self.linears[i](x)
+        x = self.linear_post(x)
+        return x
