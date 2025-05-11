0f09d7efea075c641b91b9d688e84aee28f3641d
Pedro Curvo <pedro.mpcurvo@gmail.com>
2025-05-11 21:03:10 +0200

    feat: Add __init__.py to expose Transolver models for irregular and structured meshes

---
+++ models/__init__.py
@@ -0,0 +1,9 @@
+from .Transolver_Irregular_Mesh import Model as Transolver_Irregular_Mesh
+from .Transolver_Structured_Mesh_2D import Model as Transolver_Structured_Mesh_2D
+from .Transolver_Structured_Mesh_3D import Model as Transolver_Structured_Mesh_3D
+
+__all__ = [
+    "Transolver_Irregular_Mesh",
+    "Transolver_Structured_Mesh_2D",
+    "Transolver_Structured_Mesh_3D",
+]
