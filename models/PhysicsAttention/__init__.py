4f4ece73d344ebac1fb3d95801bb540c352652d7
Pedro Curvo <pedro.mpcurvo@gmail.com>
2025-05-11 20:46:34 +0200

    feat: Add __init__.py for PhysicsAttention module to expose mesh classes

---
+++ models/PhysicsAttention/__init__.py
@@ -0,0 +1,10 @@
+# Physics Attention for Structured Mesh in 3D Space
+from .StructuredMesh3D import Physics_Attention_Structured_Mesh_3D
+from .StructuredMesh2D import Physics_Attention_Structured_Mesh_2D
+from .IrregularMesh import Physics_Attention_Irregular_Mesh
+
+__all__ = [
+    'Physics_Attention_Structured_Mesh_3D',
+    'Physics_Attention_Structured_Mesh_2D',
+    'Physics_Attention_Irregular_Mesh'
+]
