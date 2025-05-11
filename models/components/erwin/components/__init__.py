4c9ec05bde42670f0af83dfaec2f9ad86a998341
Pedro Curvo <pedro.mpcurvo@gmail.com>
2025-05-11 20:42:18 +0200

    feat: Initialize components module with imports for attention, embedding, layers, MPNN, node, and pooling

---
+++ models/components/erwin/components/__init__.py
@@ -0,0 +1,11 @@
+from .attention import BallMSA
+from .embedding import ErwinEmbedding
+from .layers import BasicLayer, ErwinTransformerBlock
+from .mpnn import MPNN
+from .node import Node
+from .pooling import BallPooling, BallUnpooling
+
+__all__ = [
+    "ErwinEmbedding", "MPNN", "BasicLayer", "ErwinTransformerBlock",
+    "BallMSA", "BallPooling", "BallUnpooling", "Node"
+]
