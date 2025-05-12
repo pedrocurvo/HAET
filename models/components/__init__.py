92b2123186f25463c0abc4e94d51bd3019284bda
Pedro Curvo <pedro.mpcurvo@gmail.com>
2025-05-12 13:47:55 +0200

    feat: Add ErwinFlashTransformer to exports in component module

--- models/components/__init__.py
+++ models/components/__init__.py
@@ -2,11 +2,13 @@ from .embedding import (PositionalEncoding, RotaryEmbedding,
                         apply_2d_rotary_pos_emb, apply_rotary_pos_emb,
                         rotate_half, timestep_embedding)
 from .erwin import ErwinTransformer
+from .erwinflash import ErwinTransformer as ErwinFlashTransformer
 from .mlp import MLP
 
 __all__ = [
     "MLP",
     "ErwinTransformer",
+    "ErwinFlashTransformer",
     "timestep_embedding",
     "rotate_half",
     "apply_rotary_pos_emb",
