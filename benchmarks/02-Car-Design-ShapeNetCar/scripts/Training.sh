fea9f535a83bad139db9eab5320e8667df3fe30f
pedrocurvo <pedro.mpcurvo@gmail.com>
2025-05-14 15:12:05 +0200

    fix: Update script paths and model names in evaluation and training scripts
    - Changed directory paths in Evaluation.sh and Training.sh to reflect the new project structure.
    - Updated the model name from Transolver to ErwinTransolver in both scripts for consistency.

--- benchmarks/02-Car-Design-ShapeNetCar/scripts/Training.sh
+++ benchmarks/02-Car-Design-ShapeNetCar/scripts/Training.sh
@@ -15,6 +15,6 @@ module load 2023
 module load CUDA/12.4.0
 
 
-cd $HOME/erwinpp/02-Car-Design-ShapeNetCar
+cd $HOME/HAET/benchmarks/02-Car-Design-ShapeNetCar
 
-srun python main.py --cfd_model=Transolver --data_dir data/shapenet_car/mlcfd_data/training_data --save_dir data/shapenet_car/mlcfd_data/preprocessed_data
+srun python main.py --cfd_model=ErwinTransolver --data_dir data/shapenet_car/mlcfd_data/training_data --save_dir data/shapenet_car/mlcfd_data/preprocessed_data
