e56c57667d2a7618be924f5f396004dbc28dd776
pedrocurvo <pedro.mpcurvo@gmail.com>
2025-06-24 16:25:05 +0200

    fix: update job name and CFD model parameters in training script

--- benchmarks/02-Car-Design-ShapeNetCar/scripts/Training.sh
+++ benchmarks/02-Car-Design-ShapeNetCar/scripts/Training.sh
@@ -2,11 +2,11 @@
 
 #SBATCH --partition=gpu_a100
 #SBATCH --gpus=1
-#SBATCH --job-name=erwin
+#SBATCH --job-name=car
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=9
-#SBATCH --time=20:00:00
-#SBATCH --output=slurm_output/slurm_output_%A.out
+#SBATCH --time=80:00:00
+#SBATCH --output=slurm_output/CAR/%A.out
 
 module purge
 module load 2024
@@ -18,11 +18,11 @@ module load CUDA/12.4.0
 cd $HOME/HAET/benchmarks/02-Car-Design-ShapeNetCar
 
 srun python main.py \
-    --cfd_model=ErwinTransolver500 \
+    --cfd_model=ErwinTransolverS64 \
     --data_dir data/shapenet_car/mlcfd_data/training_data \
     --save_dir data/shapenet_car/mlcfd_data/preprocessed_data \
+    --batch_size 1 \
     --weight 0.5 \
-    --unified_pos 1 \
-    --lr 0.00015 \
+    --unified_pos 0 \
     --nb_epochs 500 \
-    --slice_num 32
+    --slice_num 64
